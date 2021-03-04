e045c2872f394a13aa55af6e64b54254ce0acac5
yolanmees <yolan.mees@icloud.com>
2021-03-04 20:19:13 +0100

    Currency setting

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -28,7 +28,22 @@
               >
             <label for="settings_site_url" class="font-bold text-sm">Site URL</label>
             <input name="settings_site_url" class="border-2 border-gray-200 rounded w-full py-2 px-4 mb-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-800" type="text" value="{{ $settings['settings_site_url'] ?? '' }}">
-        
+
+
+            <label for="settings_currency" class="font-bold text-sm">Currency</label>
+            <select name="settings_currency" class="border-2 border-gray-200 rounded w-full py-2 px-4 mb-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-800" type="text">
+              @if (!empty($settings['settings_currency']))
+                <option value="{{$settings['settings_currency']}}">{{$settings['settings_currency']}}</option>
+                <option disabled> - </option>
+              @else
+                <option value="" selected> - </option>
+                <option value="EUR">EUR</option>
+                <option value="USD">USD</option>
+                <option value="GBP">GBP</option>
+
+              @endif
+            <select>
+
             <button class="bg-indigo-700 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded inline-flex items-center" type="submit">
                 Save
             </button>
